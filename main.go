01d1f324a27b1c75732892ee723d94cde379673e
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-14 19:49:45 -0300

    add base entrypoint

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+var (
+	token   string
+	signf   = make(chan os.Signal)
+	forever = make(chan bool)
+	client  *discordgo.Session
+)
+
+func init() {
+	godotenv.Load()
+	token = os.Getenv("TOKEN")
+}
+
+func handleInterrupt(sigch chan os.Signal) {
+	<-sigch
+	log.Println("Stopping ...")
+	client.Close()
+	forever <- true
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var err error
+	client, err = discordgo.New("Bot " + token)
+
+	if err != nil {
+		log.Panicln("Failed to connect to discord\n" + err.Error())
+	}
+
+	if err = client.Open(); err != nil {
+		log.Panicln("Failed to connect to discord\n" + err.Error())
+	}
+
+	signal.Notify(signf, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	go handleInterrupt(signf)
+
+	<-forever
 }

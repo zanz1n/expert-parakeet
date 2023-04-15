324825475f06f5a9bfff82d43b2d2a1394e5f158
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-14 21:15:36 -0300

    add basic response util

---
+++ utils/interaction.go
@@ -0,0 +1,12 @@
+package utils
+
+import "github.com/bwmarrin/discordgo"
+
+func BasicResponse(msg string) *discordgo.InteractionResponse {
+	return &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: msg,
+		},
+	}
+}

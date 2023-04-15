dd008d2a0a1a87554cb7dec4e619d3567b2a88d3
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-15 15:05:39 -0300

    add error handling to interaction listener

--- events.go
+++ events.go
@@ -15,7 +15,23 @@ func onInteraction(s *discordgo.Session, e *discordgo.InteractionCreate) {
 	log.Println(e.AppID)
 	if e.Type == discordgo.InteractionApplicationCommand {
 		if cmd, find := GetCommand(e.ApplicationCommandData().Name); find {
-			cmd.Handler(s, e)
+			s.InteractionRespond(e.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
+			})
+
+			err := cmd.Handler(s, e)
+
+			if err != nil {
+				errStr := err.Error()
+				log.Printf(
+					"Exception caught when executing command %s\t - %s",
+					cmd.Data.Name, errStr,
+				)
+				s.InteractionResponseEdit(e.Interaction, &discordgo.WebhookEdit{
+					Content: &errStr,
+				})
+			}
+
 			return
 		}
 	}

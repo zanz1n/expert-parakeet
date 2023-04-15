09e5772e4234c04c124c216f3d33c4ad2e47b4c4
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-15 11:20:44 -0300

    update events to use the new commands manager

--- events.go
+++ events.go
@@ -4,17 +4,19 @@ import (
 	"log"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/zanz1n/bot-inocente/utils"
 )
 
 func onReady(s *discordgo.Session, e *discordgo.Ready) {
 	log.Println("Connected")
+	PostCommands(s)
 }
 
 func onInteraction(s *discordgo.Session, e *discordgo.InteractionCreate) {
+	log.Println(e.AppID)
 	if e.Type == discordgo.InteractionApplicationCommand {
-		if cmd, find := utils.GetCommand(e.ApplicationCommandData().Name); find {
+		if cmd, find := GetCommand(e.ApplicationCommandData().Name); find {
 			cmd.Handler(s, e)
+			return
 		}
 	}
 }
